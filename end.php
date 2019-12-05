ab5fd9235de2fe55a3f09285d4a07ae80f94a6c8
mmohh2 <moh19951@outlook.com>
2019-12-05 14:38:43 +0100

    Create end.php

---
+++ end.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Stijn Papier Schaar</h1>
+    <hr>
+    <?php
+        // if (!isset($_GET["Player1"]) || !isset($GET["Player2"])) {
+        //     header("Location: game.php");
+        // }
+        $computer = rand(1,3);
+        if ($computer == 1) {
+            $pc2 = "Stijn";
+        }
+        if ($computer == 2) {
+            $pc2 = "Papier";
+        }
+        if ($computer == 3) {
+            $pc2 = "Schaar";
+        }
+        $speler1 = $_GET["p1c"];
+        $speler2 = $_GET["p2c"];
+        echo "<h1>Speler 1 koos " . $speler1 . ".</h1>";
+        echo "<h1>Speler 2 koos " . $speler2 . ".</h1>";
+        if ($speler1 == "Stijn") {
+            if ($speler2 == "Stijn") {
+                echo "Gelijkspel";
+            }
+            if ($speler2 == "Papier") {
+                echo "Speler 2 wint";
+            }
+            if ($speler2 == "Schaar") {
+                echo "Speler 1 wint";
+            }
+        }
+        if ($speler1 == "Papier") {
+            if ($speler2 == "Stijn") {
+                echo "Speler 1 wint";
+            }
+            if ($speler2 == "Papier") {
+                echo "Gelijkspel";
+            }
+            if ($speler2 == "Schaar") {
+                echo "Speler 2 wint";
+            }
+        }
+        if ($speler1 == "Schaar") {
+            if ($speler2 == "Stijn") {
+                echo "Speler 2 wint";
+            }
+            if ($speler2 == "Papier") {
+                echo "Speler 1 wint";
+            }
+            if ($speler2 == "Schaar") {
+                echo "Gelijkspel";
+            }
+        }
+    ?>
+</body>
+</html>
