2bcc13d0eeb0773c522d67bd51bb93a2a2853d3a
mmohh2 <moh19951@outlook.com>
2019-12-05 14:39:41 +0100

    Create start.php

---
+++ start.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stijn, Papier, Schaar</title>
+</head>
+<body>
+		<h1>Stijn Papier Schaar</h1>
+		<h2>Wil je tegen computer of speler spelen?</h2>
+        <a href="game.php">tegen speler</a>
+        <a href="computer.php">tegen computer</a>
+	
+</body>
+</html>
